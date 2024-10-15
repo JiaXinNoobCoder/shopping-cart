ddf0de111b0007f7b0a2db9833dc85dffa08cea0
liang-jiaxin1996 <jiaxinliang79@163.com>
2024-10-15 22:50:57 +0800

    add product api commit

--- src/main/java/com/jiaxin/carts/service/ProductService.java
+++
@@ -1,7 +0,0 @@
-package com.jiaxin.carts.service;
-
-import com.jiaxin.carts.entity.Product;
-
-public interface ProductService {
-    Product findProductByProductID(int productID);
-}
--- src/main/java/com/jiaxin/carts/service/UserService.java
+++
@@ -1,6 +0,0 @@
-package com.jiaxin.carts.service;
-
-import com.jiaxin.carts.entity.User;
-public interface UserService {
-    User findUserByUserID(int userID);
-}
